366ab0d1fcf433ab9a2a6f3ec3c5e73e7ab440a8
DeazStar <deadfromtheborn@gmail.com>
2025-05-04 20:31:31 +0300

    add autocomplete functionality

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,8 @@
 from enum import Enum
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class TaskStatus(Enum):
     PENDING = 'PENDING'
@@ -10,3 +13,5 @@ class TaskStatus(Enum):
 # Define the absolute path to the JSON file
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 GRAPH_INFO_PATH = os.path.join(BASE_DIR, '../Data/count_info.json')
+ES_URL = os.getenv('ES_URL')
+ES_API_KEY = os.getenv('ES_API_KEY')
